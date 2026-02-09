f21f4cbbcee850e8e24f31c2e0e57879ea0d94fd
Fola Lad <ranmiladepo@gmail.com>
2026-02-09 19:30:26 +0100

    filter out unreplyable tweets

--- src/filter.ts
+++ src/filter.ts
@@ -1,10 +1,16 @@
-
 import { Tweet } from "./types";
 import { ENGAGEMENT_FLOOR } from "./config";
 
-export function filterByFloor(
-  tweets: Tweet[],
-  floor = ENGAGEMENT_FLOOR
-): Tweet[] {
-  return tweets.filter(t => t.engagementSum >= floor);
+/**
+ * Keep tweets above engagement threshold and that are reply-able.
+ */
+export function filterByFloor(tweets: Tweet[], floor = ENGAGEMENT_FLOOR): Tweet[] {
+  return tweets.filter((t: any) => {
+    if (t.engagementSum < floor) return false;
+
+    // Skip tweets where replies are restricted
+    if (t.isLimitedReply === true) return false;
+
+    return true;
+  });
 }
